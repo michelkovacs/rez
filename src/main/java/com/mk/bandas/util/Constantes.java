d8a21676f25383ab3eea1d381c8202a360852f4f
Michel <michelmmk@gmail.com>
2017-04-12 21:30:39 -0300

    Revert "primeiro commit 12-04-2017 21:04"
    This reverts commit 6b93651a7628ef8876cfae347999bba7d963b8a1.

--- src/main/java/com/mk/bandas/util/Constantes.java
+++
@@ -1,6 +0,0 @@
-package com.mk.bandas.util;
-
-public class Constantes {
-	public static final String DIR_BASE_FOTOS = "C:\\Users\\michel\\workspace\\fotosBandas\\";
-//	public static final String DIR_BASE_FOTOS = "C:\\Users\\GISELE\\Desktop\\Michel\\workspace\\fotosBandas\\";
-}
